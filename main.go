a17aadf8617a9bce03a78ee5a82be8f109b1d590
FaizBastomi <64179402+FaizBastomi@users.noreply.github.com>
2025-05-16 21:13:23 +0700

    Adding HapusData and refactoring code (#5)
    * feat(interactive): HapusData
    
    chore(interactive): rename UbahDataMenu to UbahData
    
    * ref: can't use slice, so use fixed-size
    
    ref(utils): can't use break nor continue, so use if statement
    
    feat: filterNonEmpty, filter out empty data in fixed-size array
    
    feat: randomID, generate random identity used to identify data

--- main.go
+++ main.go
@@ -24,35 +24,37 @@ func main() {
 
 	interactive.ClearConsole()
 	for opsi != 8 {
-	fmt.Println("Select Menu:")
-	fmt.Println("1. Tambah Data")
-	fmt.Println("2. Edit Data")
-	fmt.Println("3. Hapus Data")
-	fmt.Println("4. Tampilkan Data")
-	fmt.Println("5. Daftar Polusi Tertinggi")
-	fmt.Println("6. Daftar Polusi Terendah")
-	fmt.Println("7. Cari Data")
-	fmt.Println("8. Exit")
-	fmt.Print("Masukan opsi: ")
-	fmt.Scanln(&opsi)
+		fmt.Println("Select Menu:")
+		fmt.Println("1. Tambah Data")
+		fmt.Println("2. Edit Data")
+		fmt.Println("3. Hapus Data")
+		fmt.Println("4. Tampilkan Data")
+		fmt.Println("5. Daftar Polusi Tertinggi")
+		fmt.Println("6. Daftar Polusi Terendah")
+		fmt.Println("7. Cari Data")
+		fmt.Println("8. Exit")
+		fmt.Print("Masukan opsi: ")
+		fmt.Scanln(&opsi)
 
-	switch opsi {
-	case 1:
-		interactive.TambahData(&data)
-	case 2:
-		interactive.UbahDataMenu(&data)
-	case 3:
-		interactive.HapusData(&data)
-	case 4:
-		interactive.ShowData(&data)
-	case 5:
-		interactive.UrutPolusiTertinggi(&data)
-	case 6:
-		interactive.UrutPolusiTerendah(&data)
-	case 7:
-		interactive.CariData(&data)
+		switch opsi {
+		case 1:
+			interactive.TambahData(&data)
+		case 2:
+			interactive.UbahData(&data)
+		case 3:
+			interactive.HapusData(&data)
+		case 4:
+			interactive.ShowData(&data)
+		case 5:
+			interactive.UrutPolusiTertinggi(&data)
+		case 6:
+			interactive.UrutPolusiTerendah(&data)
+		case 7:
+			interactive.CariData(&data)
+		}
+
+		interactive.ClearConsole()
 	}
-}
 
 	// Write data to JSON file
 	_ = utils.WriteToJSON(data, path+"/data.json")
